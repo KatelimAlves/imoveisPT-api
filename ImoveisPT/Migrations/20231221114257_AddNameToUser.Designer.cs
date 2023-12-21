8201e05b02b0eec43c07b2ad07bdfa5856ca6562
Katelim Alves <katelimhottz@gmail.com>
2023-12-21 16:05:18 +0000

    alteração no Aplication Users

--- ImoveisPT/Migrations/20231221114257_AddNameToUser.Designer.cs
+++ ImoveisPT/Migrations/20231221114257_AddNameToUser.Designer.cs
@@ -4,6 +4,7 @@ using ImoveisPT.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ImoveisPT.Migrations
 {
     [DbContext(typeof(ImoveisPTContext))]
-    partial class ImoveisPTContextModelSnapshot : ModelSnapshot
+    [Migration("20231221114257_AddNameToUser")]
+    partial class AddNameToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
